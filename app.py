5511f13a131156f3adce9c5ce4b537358d11600f
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2023-09-21 09:05:09 +0300

    Add files via upload
    server app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+maked_cmds = 0
+
+@app.route('/')
+def index():
+    return f"""<title>Sapbot Command Services</title>
+<p>Today maked {maked_cmds} commands</p>"""
+
+@app.route('/run', methods=['POST'])
+def run_command():
+    global maked_cmds
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    elif not request.json or not 'args' in request.json:
+        abort(400)
+    #RUN
+    cmd = request.json["name"]
+    args = request.json["args"]
+    response = None
+    if cmd == 'helloworld':
+        response = 'Hello World!'
+    elif cmd == 'echo':
+        response = args[0]
+    #RETURN
+    if response == None:
+        code = 400
+    else:
+        code = 201
+    maked_cmds += 1
+    return jsonify({'response':response}), code
+
+if __name__ == '__main__':
+    app.run(debug=True)
