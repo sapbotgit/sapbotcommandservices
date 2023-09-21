146560092285bebec53cbcedabfcfed59ed1a354
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2023-09-21 10:03:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def run_command():
     return jsonify({'response':response}), code
 
 if __name__ == '__main__':
-    app.run(host=socket.gethostbyname(socket.gethostname()), debug=True)
+    app.run(host=socket.gethostbyname(socket.gethostname()), debug=False)
